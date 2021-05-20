b9f7d5b684a59350487d0dc0bbb003fa1b14477e
rafal1018 <rafaljaneczek@o2.pl>
2021-05-20 10:20:43 +0200

    @Disable && @DisplayName

--- src/test/java/pl/devfoundry/testing/CartTest.java
+++ src/test/java/pl/devfoundry/testing/CartTest.java
@@ -1,14 +1,19 @@
 package pl.devfoundry.testing;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
+@DisplayName("Test cases for cart")
 public class CartTest {
 
+//    @Disabled
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 100 ms")
     void simulateLargeOrder() {
 
         //given
