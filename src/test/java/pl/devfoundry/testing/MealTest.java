b62dedfeedce31642ac42c386d96ef7c584ec43f
rafal1018 <rafaljaneczek@o2.pl>
2021-05-20 12:32:42 +0200

    Parametrized test

--- src/test/java/pl/devfoundry/testing/MealTest.java
+++ src/test/java/pl/devfoundry/testing/MealTest.java
@@ -1,23 +1,32 @@
 package pl.devfoundry.testing;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MealTest {
 
     @Test
     void shouldReturnDiscoumntedPrice() {
-        //givven
+        //given
         Meal meal = new Meal(35);
 
         //when
-        int discountedPrioce = meal.getDiscountedPrice(7);
+        int discountedPrice = meal.getDiscountedPrice(7);
 
         //then
-        assertEquals(28, discountedPrioce);
-        assertThat(discountedPrioce).isEqualTo(28);
+        assertEquals(28, discountedPrice);
+        assertThat(discountedPrice, equalTo(28));
 
     }
 
@@ -30,7 +39,7 @@ class MealTest {
 
         //then
         assertSame(meal1, meal2);
-        assertThat(meal1).isEqualTo(meal2);
+        assertThat(meal1, equalTo(meal2));
 
     }
 
@@ -43,7 +52,7 @@ class MealTest {
 
         //then
         assertNotSame(meal1, meal2);
-        assertThat(meal1).isNotEqualTo(meal2);
+        assertThat(meal1, not(sameInstance(meal2)));
 
     }
 
@@ -56,7 +65,7 @@ class MealTest {
 
         //then
         assertEquals(meal1, meal2, "Checking if two meals are equal");
-        assertThat(meal1).isEqualTo(meal2);
+        assertThat(meal1, not(sameInstance(meal2)));
 
     }
 
@@ -71,4 +80,37 @@ class MealTest {
         assertThrows(IllegalArgumentException.class, () -> meal.getDiscountedPrice(40));
 
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {5, 10, 15, 18})
+    void mealPricesShouldBeLowerThan20(int price) {
+        assertThat(price, lessThan(20));
+    }
+
+    @ParameterizedTest
+    @MethodSource("createMealsWithNameAndPrice")
+    void burgersShouldHaveCorrectNameAndPrice(String name, int price) {
+        assertThat(name, containsString("burger"));
+        assertThat(price, greaterThanOrEqualTo(10));
+    }
+
+    private static Stream<Arguments> createMealsWithNameAndPrice() {
+        return Stream.of(
+                Arguments.of("Hamburger", 10),
+                Arguments.of("Cheeseburger", 12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("createCakeNames")
+    void cakeNamesShouldEndWIthCake(String name) {
+        assertThat(name, notNullValue());
+        assertThat(name, endsWith("cake"));
+    }
+
+    private static Stream<String> createCakeNames() {
+        List<String> cakeNames = Arrays.asList("Cheesecake", "Fruitcake", "Capcake");
+        return cakeNames.stream();
+    }
+
 }
