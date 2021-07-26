6abd66826f94fa6c318c1a8a1ddb555e8a70a2a4
rafal1018 <rafaljaneczek@o2.pl>
2021-07-26 09:12:13 +0200

    Stubs

---
+++ src/test/java/pl/devfoundry/testing/account/AccountServiceTest.java
@@ -0,0 +1,28 @@
+package pl.devfoundry.testing.account;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+public class AccountServiceTest {
+
+
+    @Test
+    void getAllActiveAccounts() {
+
+        //given
+        AccountRepository accountRepositoryStub = new AccountRepositoryStub();
+        AccountService accountService = new AccountService(accountRepositoryStub);
+
+        //when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        //then
+        assertThat(accountList, hasSize(2));
+
+    }
+
+}
